b63b14318761a71b2a0a521b0559f338410c46e6
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-15 03:31:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ end
   end
 
   post '/puppy/' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
 
   end
