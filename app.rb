3ef08ae1eb4fc85bdd4a35fdd030ccb0b9abad08
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-15 03:38:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+
+  # get '/create_puppy' do
+  #   erb :create_puppy
+  # end
+
+#   post '/create_puppy' do
+#   @new_puppy = Puppy.new(params[:puppy])
+#
+#     erb :display_puppy
+# end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+    erb :display_puppy
+
+  end
 end
